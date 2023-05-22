68c033cb8018da18569e46b8f468902145700b31
kingfoxxx <brightally6@gmail.com>
2023-05-21 17:46:44 -0700

    path

---
+++ cmd_path.c
@@ -0,0 +1,220 @@
+#include "shell.h"
+
+
+/**
+ * is_closedir - checks ":" if is in the current directory
+ * @path: type char pointer char.
+ * @j: type int pointer of index for the file.
+ * Return: 1 if the path is searchable in the cd, 0 otherwise.
+ */
+
+int is_closedir(char *path, int *j)
+
+{
+
+	if (path[*j] == ':')
+		return (1);
+
+	while (path[*j] != ':' && path[*j])
+	{
+		*j += 1;
+	}
+
+	if (path[*j])
+		*j += 1;
+
+	return (0);
+}
+
+/**
+ * _which - locates a command
+ *
+ * @cmd: command name
+ * @_environ: environment variable
+ * Return: location of the command.
+ */
+
+char *_which(char *cmd, char **_environ)
+
+{
+	char *path, *ptr_path, *token_path, *dir;
+	int len_dir, len_cmd, i;
+	struct stat st;
+
+	path = _getenv("PATH", _environ);
+	if (path)
+	{
+		ptr_path = _strdup(path);
+		len_cmd = _strlen(cmd);
+		token_path = _strtok(ptr_path, ":");
+		i = 0;
+		while (token_path != NULL)
+		{
+			if (is_closedir(path, &i))
+				if (stat(cmd, &st) == 0)
+					return (cmd);
+			len_dir = _strlen(token_path);
+			dir = malloc(len_dir + len_cmd + 4);
+			_strcpy(dir, token_path);
+			_strcat(dir, "/");
+			_strcat(dir, cmd);
+			_strcat(dir, "\0");
+			if (stat(dir, &st) == 0)
+			{
+				free(ptr_path);
+				return (dir);
+			}
+			free(dir);
+			token_path = _strtok(NULL, ":");
+		}
+		free(ptr_path);
+		if (stat(cmd, &st) == 0)
+			return (cmd);
+		return (NULL);
+	}
+	if (cmd[0] == '/')
+		if (stat(cmd, &st) == 0)
+		{
+			return (cmd);
+		}
+	return (NULL);
+}
+
+/**
+ * is_executable - determines if is an executable by other files
+ *
+ * @datash: data structure
+ * Return: 0 if is not an executable, other number if it does
+ */
+
+int is_executable(data_shell *datash)
+
+{
+	struct stat st;
+	int i;
+	char *inputs;
+
+	inputs = datash->args[0];
+	for (i = 0; inputs[i]; i++)
+	{
+		if (inputs[i] == '.')
+		{
+			if (inputs[i + 1] == '.')
+				return (0);
+			if (inputs[i + 1] == '/')
+				continue;
+			else
+				break;
+		}
+		else if (inputs[i] == '/' && i != 0)
+		{
+			if (inputs[i + 1] == '.')
+				continue;
+			i++;
+			break;
+		}
+		else
+			break;
+	}
+	if (i == 0)
+		return (0);
+
+	if (stat(inputs + i, &st) == 0)
+	{
+		return (i);
+	}
+	get_error(datash, 127);
+	return (-1);
+}
+
+/**
+ * check_error_cmd - verifies if user has permissions to access
+ *
+ * @dir: destination directory
+ * @datash: data structure
+ * Return: 1 if there is an error, 0 if not
+ */
+
+int check_error_cmd(char *dir, data_shell *datash)
+
+{
+	if (dir == NULL)
+	{
+		get_error(datash, 127);
+		return (1);
+	}
+
+	if (_strcmp(datash->args[0], dir) != 0)
+	{
+		if (access(dir, X_OK) == -1)
+		{
+			get_error(datash, 126);
+			free(dir);
+			return (1);
+		}
+		free(dir);
+	}
+	else
+	{
+		if (access(datash->args[0], X_OK) == -1)
+		{
+			get_error(datash, 126);
+			return (1);
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * cmd_exec - executes command lines for exec function
+ *
+ * @datash: data relevant (args and input)
+ * Return: 1 on success.
+ */
+
+int cmd_exec(data_shell *datash)
+
+{
+	pid_t pds;
+	pid_t wpd;
+	int sign;
+	int exec;
+	char *dir;
+	(void) wpd;
+
+	exec = is_executable(datash);
+	if (exec == -1)
+		return (1);
+	if (exec == 0)
+	{
+		dir = _which(datash->args[0], datash->_environ);
+		if (check_error_cmd(dir, datash) == 1)
+			return (1);
+	}
+
+	pds = fork();
+	if (pds == 0)
+	{
+		if (exec == 0)
+			dir = _which(datash->args[0], datash->_environ);
+		else
+			dir = datash->args[0];
+		execve(dir + exec, datash->args, datash->_environ);
+	}
+	else if (pds < 0)
+	{
+		perror(datash->av[0]);
+		return (1);
+	}
+
+	else
+	{
+		do {
+			wpd = waitpid(pds, &sign, WUNTRACED);
+		} while (!WIFEXITED(sign) && !WIFSIGNALED(sign));
+	}
+
+	datash->status = sign / 256;
+	return (1);
+}
