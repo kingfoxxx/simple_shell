d404e84405c037a058aa30fe92b2cde888557d4a
kingfoxxx <brightally6@gmail.com>
2023-05-24 23:06:21 -0700

    simple shell

---
+++ envec_line.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+
+/**
+ * exec_line - lets find builtins and commands lines for file
+ *
+ * @datash: data relevant (args) in lines
+ * Return: 1 on success.
+ */
+
+int exec_line(data_shell *datash)
+
+{
+
+	int (*builtin)(data_shell *datash);
+
+	if (datash->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(datash->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(datash));
+
+	return (cmd_exec(datash));
+}
